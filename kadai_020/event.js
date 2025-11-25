de60ede502456f9ea25003a1e9d071104b841b4e
Riwa88 <like.a.box.of.chocolatee@gmail.com>
2025-11-25 22:12:35 +0900

    DOM操作を使用して文字表示を変えようをアップデート

---
+++ kadai_020/event.js
@@ -0,0 +1,5 @@
+const eventBtn = document.getElementById('btn');
+const eventText = document.getElementById('text');
+eventBtn.addEventListener('click', () => {
+    eventText.textContent = 'ボタンをクリックしました';
+});
